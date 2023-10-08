926507fef1a7c88405c1c7974d684bc8da37b7b3
Jeremy Garcia <73523692+jareuhmee@users.noreply.github.com>
2023-10-08 00:28:44 -0400

    clean up

--- mental-break/home.py
+++ mental-break/home.py
@@ -1,24 +1,19 @@
 # Importing the Streamlit library
 import streamlit as st
-from google.cloud import language_v1
 import streamlit_authenticator as stauth
+from google.cloud import language_v1
 
-from pymongo.mongo_client import MongoClient
+# Page configurations
+st.set_page_config(
+    page_title="Home"
+)
 
 # Streamlit app title
-st.title('Mental Breakdown')
-
+st.title("Mental Breakdown")
+st.write("A journaling web application that analyzes your daily journal entries and provides feedback on ways to improve the next day.")
 
 
 # Login
-
-# Connect to your MongoDB database
-# client = MongoClient("mongodb+srv://<username>:<password>@<cluster-url>/<dbname>?retryWrites=true&w=majority")
-# db = client['<dbname>']
-
-# # Define a collection for user data
-# users = db['users']
-
 authenticator = stauth.Authenticate(
     dict(st.secrets['credentials']).copy(),
     st.secrets['cookie']['name'],
@@ -28,15 +23,4 @@ authenticator = stauth.Authenticate(
 )
 
 name, authentication_status, username = authenticator.login('Login', 'main')
-print(name, authentication_status, username)
-
-# Authenticating users
-if st.session_state["authentication_status"]:
-    authenticator.logout('Logout', 'main', key='unique_key')
-    st.write(f'Welcome *{st.session_state["name"]}*')
-    st.title('Some content')
-    print("test")
-elif st.session_state["authentication_status"] is False:
-    st.error('Username/password is incorrect')
-elif st.session_state["authentication_status"] is None:
-    st.warning('Please enter your username and password')
+# print(name, authentication_status, username)
