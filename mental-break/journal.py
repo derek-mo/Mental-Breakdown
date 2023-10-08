64c6403a30fae8285668c97e5033365fac7479f5
Derek Mo <derek.mo7203@gmail.com>
2023-10-08 06:08:10 -0400

    datafram for journaling specific data

--- mental-break/journal.py
+++ mental-break/journal.py
@@ -1,83 +1,111 @@
 import streamlit as st
+import pandas as pd
 from google.cloud import language_v1
+from streamlit_extras.chart_container import chart_container
+from streamlit_extras.chart_container import _get_random_data
 
 # configure the page
-# st.set_page_config(
-#     page_title="Journal",
-#     page_icon="📘",
-# )
-
-def show(name, username):
-    # contains all data for an entry
-    class Entry:
-        title = ""
-        content = ""
-
-        def __init__(self, title, content):
-            self.title = title
-            self.content = content
+st.set_page_config(
+    page_title="Journal",
+    page_icon="📘",
+)
 
 
-    def analyze(entry: Entry):
-        # ======== GOOGLE CLOUD NLP ========
-        # Instantiates a client
-        client = language_v1.LanguageServiceClient()
+# contains all data for an entry
+class Entry:
+    title = ""
+    content = ""
+    score = 0
 
-        # The text to analyze
-        text = entry.content
-        document = language_v1.types.Document(
-            content=text, type_=language_v1.types.Document.Type.PLAIN_TEXT
-        )
+    def __init__(self, title, content, score):
+        self.title = title
+        self.content = content
+        self.score = score
 
-        # Detects the sentiment of the text
-        sentiment = client.analyze_sentiment(
-            request={"document": document}
-        ).document_sentiment
+def calcScore(content : str):
+    # ======== GOOGLE CLOUD NLP ========
+    # Instantiates a client
+    client = language_v1.LanguageServiceClient()
 
-        print(f"Text: {text}")
-        print(f"Sentiment: {sentiment.score}")
+    # The text to analyze
+    text = content
+    document = language_v1.types.Document(
+        content=text, type_=language_v1.types.Document.Type.PLAIN_TEXT
+    )
 
-        return sentiment.score
+    # Detects the sentiment of the text
+    sentiment = client.analyze_sentiment(
+        request={"document": document}
+    ).document_sentiment
 
-    st.write(f"## Welcome {name} to Your Journal! 👋")
-    tab1, tab2 = st.tabs(["New", "History"])
+    if sentiment.score > 0.7:
+        print("Extremely Positive")
+    elif sentiment.score > 0.4:
+        print("Average")
+    elif sentiment.score > 0.2:
+        print("Poor")
+    print(f"Text: {text}")
+    print(f"Sentiment: {sentiment.score}")
 
-    # creates a list of entries in session state if it doesn't exist
-    if ("entries" not in st.session_state):
-        st.session_state["entries"] = []
+    return round(sentiment.score, 2)
 
-    # contains the code for the "New" tab
-    with tab1:
+st.write("# Welcome to Your Journal! 👋")
+tab1, tab2, tab3 = st.tabs(["New", "History", "Analysis"])
+
+# creates a list of entries in session state if it doesn't exist
+if ("entries" not in st.session_state):
+    st.session_state["entries"] = []
+
+# contains the code for the "New" tab
+with tab1:
+    col1, col2 = st.columns(2)
+    with col1:
         st.write("### New Entry")
         # create text boxes for the entry
         title = st.text_input("Write a title.")
         content = st.text_area("Write the content.")
         # checks if the submitted entry is valid
-        if (st.button("Submit")):
-            if (len(title) == 0 and len(content) == 0):
+        if st.button("Submit"):
+            if(len(title) == 0 and len(content) == 0):
                 st.error("Invalid entry.")
-            elif (len(title) == 0):
+            elif(len(title) == 0):
                 st.error("Invalid title.")
-            elif (len(content) == 0):
+            elif(len(content) == 0):
                 st.error("Invalid content")
             else:
-                st.session_state["entries"].append(Entry(title, content))
+                st.session_state["entries"].append(Entry(title, content, calcScore(content)))
                 st.success("Saved new entry.")
-                st.success(analyze(st.session_state["entries"][0]))
+                st.success(st.session_state["entries"][len(st.session_state["entries"])- 1].score)
+                
 
-    # contains the code for the "History" tab
-    with tab2:
-        st.write("### History")
-        # writes the data from the entries
-        for i in range(len(st.session_state["entries"]) - 1, -1, -1):
-            entry = st.session_state["entries"][i]
-            with st.expander("**" + entry.title + "**"):
-                st.write(entry.content)
-                col1, col2, NULL = st.columns([1, 1, 3])
-                with col1:
-                    st.button("Modify", key = "modify" + str(i))
-                    # TODO: Modify Button Functionality
-                    st.button("Delete", key = "delete" + str(i))
-                    # TODO: Delete Button Functionality
-            
-    # st.session_state 
+# contains the code for the "History" tab
+with tab2:
+    st.write("### History")
+    # writes the data from the entries
+    for i in range(len(st.session_state["entries"]) - 1, -1, -1):
+        entry = st.session_state["entries"][i]
+        with st.expander("**" + entry.title + "**"):
+            st.write(entry.content)
+            col1, col2, NULL = st.columns([1, 1, 3])
+            with col1:
+                st.button("Modify", key = "modify" + str(i))
+                # TODO: Modify Button Functionality
+                st.button("Delete", key = "delete" + str(i))
+                # TODO: Delete Button Functionality
+
+with tab3:
+    st.write("### Analysis")
+
+    chart_data = {"Column 1": [], "Column 2": []}
+
+    for i in range(0, len(st.session_state["entries"])):
+        chart_data["Column 1"].append(i)
+        chart_data["Column 2"].append(st.session_state["entries"][i].score)
+        
+    df = pd.DataFrame(chart_data)
+
+    with chart_container(df):
+        st.write("Here's a cool chart")
+        st.area_chart(df)
+        
+# st.session_state 
