832a3b9de2b3b85538999f5d18c64f44abf0090b
Sharika Khondaker <sharika.khondaker@gmail.com>
2023-10-07 22:46:39 -0400

    working on resources

--- mental-break/pages/resources.py
+++ mental-break/pages/resources.py
@@ -1,3 +1,42 @@
 # Importing the Streamlit library
 import streamlit as st
+from streamlit_card import card
 
+# Resources Page
+st.title(" :mending_heart: HealthyHub :mending_heart:")
+st.write("## this is a link [we love](https://google.com)")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    try:
+        card(
+            title="Asians Mental Health Collective",
+            text="Some description",
+            image="http://placekitten.com/300/250",
+            url="https://www.google.com",
+        )
+    except:
+        st.write("")
+
+    try:
+        card(
+            title="happyhapu",
+            text="Some description",
+            image="http://placekitten.com/300/250",
+            url="https://www.google.com",
+        )
+    except:
+        st.write("")
+
+
+with col2:
+    try:
+        card(
+            title="I love your mom",
+            text="Some description",
+            image="http://placekitten.com/300/250",
+            url="https://www.google.com",
+        )
+    except:
+        st.write("")
